c8b71f23b6458c418ded077d8f3bbef1ee6acdd8
Ulysses Zheng <ulysses147@gmail.com>
2019-04-28 15:16:36 -0700

    Updates

---
+++ precache-manifest.98eb3bf9b162eae6da0ae6488c8caec8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/repo-name/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "8b0de20c703bbbf92fc6",
+    "url": "/repo-name/static/css/main.76b3cf21.chunk.css"
+  },
+  {
+    "revision": "228ec6cf0a9ecfe85cb7",
+    "url": "/repo-name/static/js/runtime~main.2586f5e2.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/repo-name/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "5a0a0a9dd51defe909fe",
+    "url": "/repo-name/static/js/2.11b3ac09.chunk.js"
+  },
+  {
+    "revision": "8b0de20c703bbbf92fc6",
+    "url": "/repo-name/static/js/main.15fda949.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/repo-name/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/repo-name/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/repo-name/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "5a0a0a9dd51defe909fe",
+    "url": "/repo-name/static/css/2.70dcc57f.chunk.css"
+  },
+  {
+    "revision": "c97c9ca64f5d3a4668c1cbebc9c5a954",
+    "url": "/repo-name/index.html"
+  }
+];
