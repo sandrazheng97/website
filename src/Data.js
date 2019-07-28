3dba63d3c97d06b5fb35d1cf2b64251bef73e7f9
Sandra Zheng <sandra@sandras-mbp.attlocal.net>
2019-07-28 16:57:29 -0700

    Added images

--- src/Data.js
+++ src/Data.js
@@ -1,74 +1,79 @@
-const elements = [
-  {
-    primary: "Vancouver",
-    secondary: "blah",
-    src: "images/finalmap.jpg"
-  },
-  {
-    primary: "blah",
-    secondary: "blah",
-    src: "images/fishcake.jpg"
-  },
-  {
-    src: "images/c.jpg"
-  },
-  {
-    height: 6,
-    src: "images/d.jpg"
-  },
-  {
+const elements = [{
+    primary: "Wander",
+    secondary: "Digital",
+    src: "images/wander.jpg",
     width: 2,
     height: 2,
-    src: "images/b.jpg"
-  },
-  {
-    src: "images/e.jpg"
-  },
-  {
-    src: "images/f.jpg"
-  },
-  {
-    src: "images/a.jpg"
-  },
-  {
-    src: "images/c.jpg"
-  },
-  {
+}, {
+    primary: "Discover",
+    secondary: "Digital",
+    src: "images/discover.jpg",
+    width: 2,
+    height: 2,
+}, {
+    primary: "Dream",
+    secondary: "Digital",
+    src: "images/dream.jpg",
+    width: 2,
+    height: 2,
+}, {
+    primary: "Radio Snake",
+    secondary: "Watercolor",
+    src: "images/radio_snake.jpg",
+    width: 3,
     height: 4,
-    src: "images/d.jpg"
-  },
-  {
-    width: 4,
+}, {
+    primary: "Extinct Nature",
+    secondary: "Ink & Watercolor",
+    width: 3,
+    src: "images/whale.jpg"
+}, {
+    primary: "Mug Towers",
+    secondary: "Ink & Watercolor",
+    src: "images/towers.jpg",
+    width: 3,
+    height: 2,
+}, {
+    primary: "Egg-Plant",
+    secondary: "Digital",
+    src: "images/eggplant.jpg",
+    width: 3,
+    height: 2,
+}, {
+    primary: "Vancouver",
+    secondary: "Digital",
+    src: "images/finalmap.jpg",
+    width: 3,
     height: 4,
-    src: "images/b.jpg"
-  },
-  {
-    src: "images/e.jpg"
-  },
-  {
-    src: "images/f.jpg"
-  },
-  {
-    src: "images/a.jpg"
-  },
-  {
-    height: 4,
-    src: "images/d.jpg"
-  },
-  {
-    width: 4,
-    height: 4,
-    src: "images/b.jpg"
-  },
-  {
-    src: "images/c.jpg"
-  },
-  {
-    src: "images/e.jpg"
-  },
-  {
-    src: "images/f.jpg"
-  }
-];
+}, {
+    primary: "Fish Cake",
+    secondary: "Intaglio Print",
+    width: 3,
+    src: "images/fishcake.jpg"
+}, {
+    primary: "Creature Comforts",
+    secondary: "Pen, Watercolor, & Digital",
+    width: 3,
+    height: 6,
+    src: "images/creature_comforts.jpg"
+}, {
+    primary: "Living Through Two Shootings",
+    secondary: "Digital",
+    width: 3,
+    height: 2,
+    src: "images/twoshootings.jpg"
+}, {
+    primary: "Musical Quintet",
+    secondary: "Ink & Digital",
+    src: "images/quintet.jpg",
+    width: 3,
+    height: 2,
+}, {
+    primary: "Sleeping Dogs",
+    secondary: "Silkscreen Print",
+    width: 3,
+    height: 2,
+    src: "images/sleeping_dog.jpg"
+}, ];
 
 export default elements;
---
+++ src/DesignData.js
@@ -0,0 +1,61 @@
+const elements = [{
+    primary: "Creature Comforts",
+    secondary: "Exhibt Poster Design",
+    src: "images/poster_ad.jpg",
+    width: 6,
+    height: 3,
+}, {
+    primary: "Floral",
+    secondary: "Mural Design",
+    src: "images/sweet_compound.jpg",
+    width: 6,
+    height: 3,
+}, {
+    primary: "Wander, Discover, Dream",
+    secondary: "3 Series Illustration",
+    src: "images/3_windows.jpg",
+    width: 3,
+    height: 3,
+}, {
+    primary: "Day Time",
+    secondary: "Throw Pillow Design",
+    src: "images/owl_pillow.jpg",
+    width: 3,
+    height: 3,
+}, {
+    primary: "Night Time",
+    secondary: "Throw Pillow Design",
+    src: "images/bunny_pillow.jpg",
+    width: 3,
+    height: 3,
+}, {
+    primary: "Hunter Green",
+    secondary: "Front & Back Cover Illustration",
+    src: "images/turtle_cover.jpg",
+    width: 4,
+    height: 2,
+}, {
+    primary: "Tomorrow Land",
+    secondary: "Poster Event Design",
+    src: "images/turtle_cover.jpg",
+    width: 4,
+    height: 2,
+}, {
+    primary: "Tomorrow Land",
+    secondary: "Poster Event Design",
+    src: "images/tmrland_mockup.jpg",
+    width: 2,
+    height: 4,
+}, {
+    primary: "Sleeping Dogs",
+    secondary: "Tote Bag Design",
+    src: "images/dog_tote.jpg",
+    width: 2,
+    height: 4,
+}, {
+    primary: "Wander",
+    secondary: "Digital",
+    src: "images/scented_candle.jpg",
+    width: 4,
+    height: 3,
+}, ]
