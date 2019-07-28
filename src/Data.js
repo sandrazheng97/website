38f58e31e2e3abad6b589ee65653d82f667d2965
Sandra Zheng <sandra@sandras-mbp.attlocal.net>
2019-07-28 13:57:25 -0700

    Some images

--- src/Data.js
+++ src/Data.js
@@ -1,17 +1,29 @@
 const elements = [
   {
+    primary: "Vancouver",
+    secondary: "blah",
+    src: "images/finalmap.jpg"
+  },
+  {
+    primary: "blah",
+    secondary: "blah",
+    src: "images/fishcake.jpg"
+  },
+  {
+    primary: "blah",
+    secondary: "blah",
     src: "images/a.jpg"
   },
   {
     src: "images/c.jpg"
   },
   {
-    height: 4,
+    height: 6,
     src: "images/d.jpg"
   },
   {
-    width: 4,
-    height: 4,
+    width: 2,
+    height: 2,
     src: "images/b.jpg"
   },
   {
