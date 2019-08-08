213f913dc70d0cd42e413fd87a7b49caefe06756
Ulysses Zheng <ulysses147@gmail.com>
2019-08-08 00:23:48 -0700

    AboutPage responsive part 1/2

--- src/AboutPage.js
+++ src/AboutPage.js
@@ -4,10 +4,36 @@ import LazyLoad from "react-lazyload";
 import styles from "./AboutPage.module.css";
 
 class AboutPage extends Component {
-    render() {
-        const image = "/images/sleeping_village.jpg";
-        return (
-            <div className={styles.container}>
+  constructor(props) {
+    super(props);
+    this.state = {
+      width: window.innerWidth,
+      height: window.innerHeight
+    };
+
+    this.handleResize = this.handleResize.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener("resize", this.handleResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleResize);
+  }
+
+  handleResize(event) {
+    this.setState({
+      width: window.innerWidth,
+      height: window.innerHeight
+    });
+  }
+
+  render() {
+    const { width } = this.state;
+    const image = "/images/sleeping_village.jpg";
+    return (
+      <div className={styles.container}>
         <div className={styles.pictureContainer}>
           <div className={styles.picture}>
             <LazyLoad>
@@ -21,7 +47,7 @@ class AboutPage extends Component {
           <div className={styles.infoBody}>
             Sandra Zheng is a Canadian born illustrator and designer who
             recently graduated from ArtCenter College of Design. She is based in
-            Vancouver, BC. Her illustrations usually depict nature with playful 
+            Vancouver, BC. Her illustrations usually depict nature with playful
             interactions of her own whimsical characters.
           </div>
           <div className={styles.resume}>
@@ -31,8 +57,8 @@ class AboutPage extends Component {
           </div>
         </div>
       </div>
-        );
-    }
+    );
+  }
 }
 
 export default AboutPage;
