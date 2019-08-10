aea445939fff5bf4b9f7e1ed3301adc45e146abd
Ulysses Zheng <ulysses147@gmail.com>
2019-08-09 23:21:34 -0700

    Clean up carousel.css

--- src/Constants.js
+++ src/Constants.js
@@ -3,7 +3,6 @@ const Constants = {
   copyrightHeight: 60,
   thumbnailFullContainerHeight: 150,
   thumbnailSmallContainerHeight: 50,
-  arrowsContainerHeight: 40,
   cancelContainerHeight: 25,
   numColumns: 6,
   columnSpacing: 10,
