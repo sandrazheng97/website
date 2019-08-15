0e0e8e1d006e63adb12696b81995c2abc59c4110
Sandra Zheng <sandra@sandras-mbp.attlocal.net>
2019-08-14 22:38:33 -0700

    About page formatting

--- src/AboutPage.js
+++ src/AboutPage.js
@@ -4,39 +4,39 @@ import ClassNames from "classnames";
 import styles from "./AboutPage.module.css";
 
 class AboutPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      width: window.innerWidth,
-      height: window.innerHeight,
-      isAboutPage: props.match.path === "/about"
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            width: window.innerWidth,
+            height: window.innerHeight,
+            isAboutPage: props.match.path === "/about"
+        };
 
-    this.handleResize = this.handleResize.bind(this);
-  }
+        this.handleResize = this.handleResize.bind(this);
+    }
 
-  componentDidMount() {
-    window.addEventListener("resize", this.handleResize);
-  }
+    componentDidMount() {
+        window.addEventListener("resize", this.handleResize);
+    }
 
-  componentWillUnmount() {
-    window.removeEventListener("scroll", this.handleResize);
-  }
+    componentWillUnmount() {
+        window.removeEventListener("scroll", this.handleResize);
+    }
 
-  handleResize(event) {
-    this.setState({
-      width: window.innerWidth,
-      height: window.innerHeight
-    });
-  }
+    handleResize(event) {
+        this.setState({
+            width: window.innerWidth,
+            height: window.innerHeight
+        });
+    }
 
-  render() {
-    const { width, isAboutPage } = this.state;    const image = "/images/sleeping_village.jpg";
-    const showHorizontal = width >= 600;
-    var infoContent;
-    if (isAboutPage) {
-      infoContent = (
-        <div
+    render() {
+        const { width, isAboutPage } = this.state;
+        const image = "/images/sleeping_village.jpg";
+        const showHorizontal = width >= 600;
+        var infoContent;
+        if (isAboutPage) {
+            infoContent = (<div
           className={ClassNames({
             [styles.info]: true,
             [styles.infoHorizontal]: showHorizontal
@@ -49,7 +49,8 @@ class AboutPage extends Component {
             My art is primarily inspired by nature and 
             animals that usually depict playful interactions of whimiscal characters in their
             own colorful world.
-            
+          </div>
+          <div className={styles.infoBody}>
             For jobs, commission enquiries, or simply to say hi: 
             sandrazhengart@gmail.com
           </div>
@@ -58,11 +59,10 @@ class AboutPage extends Component {
               Resume
             </a>
           </div>
-        </div>
-      );
-    } else {
-      infoContent = (
-        <div
+        </div>);
+        } else {
+            infoContent = (
+                <div
           className={ClassNames({
             [styles.info]: true,
             [styles.infoHorizontal]: showHorizontal
@@ -74,11 +74,11 @@ class AboutPage extends Component {
             <p>Please check back soon.</p>
           </div>
         </div>
-      );
-    }
+            );
+        }
 
-    return (
-      <div
+        return (
+            <div
         className={ClassNames({
           [styles.container]: true,
           [styles.containerVertical]: !showHorizontal
@@ -93,8 +93,8 @@ class AboutPage extends Component {
         />
         {infoContent}
       </div>
-    );
-  }
+        );
+    }
 }
 
 export default AboutPage;
